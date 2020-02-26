f7a7fa485a06b0c24400d493fe5cd431812ba2e3
Jizong Liang <Liang_Jizong@comcast.com>
2020-02-25 22:44:36 -0500

    completed task one

--- appendix_B/practice_comparision.js
+++ appendix_B/practice_comparision.js
@@ -1,18 +1,30 @@
+// scheduleMeeting(..) should take a start time (in 24-hour
+//  format as a string "hh:mm") and a meeting duration (number of minutes).
+//  It should return true if the meeting falls entirely within the work day
+//  (according to the times specified in dayStart and dayEnd); return false
+// if the meeting violates the work day bounds.
+
 const dayStart = "07:30";
 const dayEnd = "17:45";
 
 function scheduleMeeting(startTime, durationMinutes) {
   let [hours, minutes] = startTime.split(":");
-  hours = parseInt(hours);
-  minutes = parseInt(minutes);
-  if (hours - parseInt(dayStart)) {
-    console.log("inside");
-    console.log(minutes > parseInt(dayStart.slice(-2)));
-    return minutes > parseInt(dayStart.slice(-2));
-  }
-  //   console.log(dayStart.slice(-2));
-  console.log(hours > parseInt(dayStart) && hours < parseInt(dayEnd));
-  // return hours < parseInt(dayStart) || hours > parseInt(dayEnd);
+  let [startHr, startMin] = dayStart.split(":");
+  let [endHr, endMin] = dayEnd.split(":");
+  let totalMinutes = hours * 60 + +minutes;
+  let totalStartMins = startHr * 60 + +startMin;
+  let totalEndMins = endHr * 60 + +endMin;
+  totalMinutes > totalEndMins
+    ? console.log(false)
+    : totalMinutes === totalEndMins
+    ? console.log(true)
+    : totalMinutes === totalStartMins
+    ? console.log(true)
+    : totalMinutes < totalStartMins
+    ? console.log(false)
+    : totalMinutes + durationMinutes <= totalEndMins
+    ? console.log(true)
+    : console.log(false);
 }
 
 scheduleMeeting("7:00", 15); // false
